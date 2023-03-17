16ac2ce66d8e38bb5d8138e40cdf764e23bfdd9e
Endaxe <94972517+Endaxe@users.noreply.github.com>
2023-03-17 11:08:39 +0100

    2023.03.17

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,10 +1,30 @@
+
 public class Komplex {
 
     double a;
     double b;
 
-    public Komplex (double a, double b) {
+    double c;
+    double d;
+
+
+    public Komplex(double a, double b) {
         this.a = a;
         this.b = b;
+
+        Komplex a = new Komplex(3.0, 2.0);
+        Komplex b = new Komplex(4.0, 1.0);
+
+        Komplex c = a.add(b);
+        Komplex d = new Komplex(7, 3);
+
+        Komplex c = a.add(b);
+        c = a.multiply(b);
+
+        System.out.println(c);
+        if (c.equals(d)) {
+            System.out.println("add funkar!!");
+
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,8 @@ public class Main {
             System.out.println("add funkar!!");
         }
 
+
+
         c = a.multiply(b);
         d = new Main(10, 11);
 
